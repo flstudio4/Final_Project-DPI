33e9f95f64129937e44e27ce0e2fdbe9c8f2817d
Dmitrii Sumenko <64180264+flstudio4@users.noreply.github.com>
2023-10-26 05:14:49 +0000

    continue on UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:home, :about, :help]
   skip_forgery_protection
 end
