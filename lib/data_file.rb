ddaa2fca881056ef8a42bd39174d6ef6ac4ded53
Dmitrii Sumenko <64180264+flstudio4@users.noreply.github.com>
2023-10-28 00:57:25 +0000

    prepared unique names and added them to arrays

---
+++ lib/data_file.rb
@@ -0,0 +1,37 @@
+class DataFile
+  
+  # Thanks ChatGpt for providing names for me, despite resisting and trying to convonce me to use Faker
+
+  $male_names = [
+    "Adam", "Aiden", "Alex", "Alexander", "Andrew", "Anthony", "Archer", "Asher", "Benjamin", "Caleb", "Cameron",
+    "Carter", "Charles", "Christopher", "Daniel", "David", "Declan", "Dominic", "Eli", "Elias", "Elijah", "Eliott",
+    "Emmett", "Ethan", "Everett", "Felix", "Gabriel", "George", "Henry", "Hudson", "Hunter", "Isaac", "Jack", "Jackson",
+    "Jacob", "James", "Jason", "John", "Jonathan", "Joseph", "Joshua", "Josiah", "Julian", "Kaden", "Landon", "Leo",
+    "Liam", "Lincoln", "Logan", "Lucas", "Mason", "Matthew", "Maxwell", "Michael", "Nathan", "Nicholas", "Noah", "Oliver",
+    "Oscar", "Owen", "Patrick", "Samuel", "Sebastian", "Theodore", "Thomas", "Tristan", "Victor", "William", "Wyatt",
+    "Xander", "Zachary", "Zane", "Aleksandr", "Andrey", "Anton", "Artem", "Dmitri", "Fedor", "Ivan", "Kirill",
+    "Konstantin", "Maksim", "Mikhail", "Nikita", "Nikolay", "Pavel", "Sergey", "Stanislav", "Vadim", "Viktor", "Vladimir",
+    "Yuri", "Aleksandr", "Alexei", "Anatoli", "Andrei", "Anton", "Arkadi", "Boris", "Dmitri", "Evgeni", "Fyodor",
+    "Grigori", "Igor", "Ilya", "Leonid", "Mikhail", "Nikolai", "Pavel", "Pyotr", "Sergei", "Viktor", "Vladimir", "Yuri",
+    "Yuriy", "Yury"
+]
+
+  $female_names = [
+    "Magdalina", "Abigail", "Ada", "Adriana", "Aisha", "Alejandra", "Alexandra", "Alice", "Alina", "Alicia", "Amelia", "Ana",
+    "Anastasia", "Angelina", "Anna", "Annabelle", "Aria", "Ariana", "Aurora", "Ava", "Bella", "Brianna", "Camila",
+    "Caroline", "Charlotte", "Chloe", "Clara", "Cora", "Daisy", "Eleanor", "Elena", "Eliza", "Elizabeth", "Ella",
+    "Emilia", "Emily", "Emma", "Eva", "Evelyn", "Faith", "Fiona", "Gabriela", "Grace", "Hannah", "Harper", "Hazel",
+    "Isabella", "Ivy", "Jasmine", "Josephine", "Julia", "Katherine", "Layla", "Leah", "Lena", "Lillian", "Lily", "Luna",
+    "Lydia", "Mackenzie", "Madeline", "Madison", "Makayla", "Mila", "Natalie", "Nora", "Olivia", "Paisley", "Penelope",
+    "Piper", "Riley", "Ruby", "Samantha", "Savannah", "Scarlett", "Serenity", "Sofia", "Sophia", "Sophie", "Stella",
+    "Valentina", "Victoria", "Violet", "Zoe", "Anastasia", "Daria", "Ekaterina", "Elena", "Elizaveta", "Galina", "Irina",
+    "Ksenia", "Larisa", "Ludmila", "Margarita", "Maria", "Nadezhda", "Natalia", "Olga", "Polina", "Svetlana", "Tatiana",
+    "Valentina", "Varvara", "Vera", "Veronika", "Viktoria", "Yelena", "Aaliyah", "Adalynn", "Adele", "Adrienne", "Alexia",
+    "Aliyah", "Amelie", "Angelica", "Arabella", "Beatrice", "Bianca", "Camille", "Catalina", "Cecilia", "Celeste",
+    "Delilah", "Evangeline", "Francesca", "Genevieve", "Gianna", "Guinevere", "Haven", "Imogen", "Isabeau", "Jocelyn",
+    "Kamila", "Karina", "Katrina", "Leonora", "Liliana", "Luciana", "Luz", "Mallory", "Margot", "Marina", "Melody",
+    "Miranda", "Nadia", "Nayeli", "Noelle", "Ophelia", "Rosalie", "Seraphina", "Sienna", "Sylvia", "Talia", "Viviana",
+    "Wren", "Zara"
+]
+
+end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,10 +1,9 @@
+require 'data_file'
+
 desc "Fill the database tables with some sample data"
 task({ :sample_data => :environment }) do
-  if Rails.env.development?
-    User.destroy_all
+  157.times do
+    
+    
   end
-  user = User.new(email: "alice@example.com", username: "alice", password: "password", password_confirmation: "password")
-  user.save
-
-  
 end
