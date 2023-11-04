1137d540d269167ba7da82e1d3d6b9865d58bbef
D.S <64180264+flstudio4@users.noreply.github.com>
2023-11-04 13:08:32 -0500

    changed permissions for form fields for account registration and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :gender, :avatar, :country, :state, :city, :dob, :bio)}
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :avatar, :country, :state, city, :bio, :password, :current_password)}
   end
 end
