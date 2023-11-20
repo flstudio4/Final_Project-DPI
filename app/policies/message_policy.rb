ed93bcb1c79237b944ef410a7bba2c1700a949c3
D.S <64180264+flstudio4@users.noreply.github.com>
2023-11-19 20:31:37 -0600

    commented out pundit for testing in production

--- app/policies/message_policy.rb
+++ app/policies/message_policy.rb
@@ -1,5 +1,5 @@
-# class MessagePolicy < ApplicationPolicy
-#   def create?
-#     !Block.exists?(blocker_id: record.chat.receiver_id, blocked_id: user.id)
-#   end
-# end
+class MessagePolicy < ApplicationPolicy
+  def create?
+    !Block.exists?(blocker_id: record.chat.receiver_id, blocked_id: user.id)
+  end
+end
