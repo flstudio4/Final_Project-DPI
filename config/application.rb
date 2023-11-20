2985087bcdb0e524a6cc723715a778bb46248b14
D.S <64180264+flstudio4@users.noreply.github.com>
2023-11-19 20:13:03 -0600

    changes

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module RailsTemplate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.autoload_paths += Dir[Rails.root.join('app', 'policies', '*.rb')]
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Configuration for the application, engines, and railties goes here.
     #
